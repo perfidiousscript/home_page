909313de7eea1e4dcb26ede21a5cc496ae6c6f7f
Sam Moss <sam.moss321@gmail.com>
2021-01-17 19:28:54 -0800

    actually gets ergot image centered

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <>
-    <div className="landing">
-      <Image src="/ergot.jpg" width="800" height="400" />
-    </div>
+      <div className={styles.landing}>
+        <Link href="/bio">
+          <Image src="/ergot.jpg" width="800" height="400" />
+        </Link>
+      </div>
     </>
   )
 }
