4b76554f260724065427ebffaedcad5cd81c8767
Sam Moss <sam.moss321@gmail.com>
2021-01-18 09:12:20 -0800

    bio stlying

--- pages/bio.js
+++ pages/bio.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Bio.module.css";
 import Link from "next/link";
 import Header from "../components/header.js";
 
@@ -9,13 +9,18 @@ export default function Bio() {
     <>
       <Header />
       <div className={styles.bio}>
-        <Image src="/bio_img.jpg" width="300" height="300" />
-        <p>
+        <Image
+          className={styles.bioImage}
+          src="/bio_img.jpg"
+          width="300"
+          height="300"
+        />
+        <div className={styles.bioBody}>
           Samuel M. Moss is from Cascadia. His work has been published in{" "}
           <i>3:AM Magazine</i>, <i>Young Mag</i>, <i>Word For/Word</i> and{" "}
           <i>Vastarien</i> among other venues. He is an associate editor and web
           lead at 11:11 Press.
-        </p>
+        </div>
       </div>
     </>
   );
