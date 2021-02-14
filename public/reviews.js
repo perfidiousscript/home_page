e6028cb3a1101d4a024c002e6c9ed6db2ecd2592
Sam Moss <sam.moss321@gmail.com>
2021-02-14 10:56:49 -0800

    adds directory to reviews

--- public/reviews.js
+++ public/reviews.js
@@ -1,32 +1,104 @@
 export const reviews = {
   default: {
     title: null,
-    body:
-      ["I review small press innovative fiction. If you have an upcoming release you would like reviewed, feel free to contact me at thisis.fryou@gmail.com",]
+    body: [
+      "I review small press innovative fiction. If you have an upcoming or recent release you would like reviewed, feel free to contact me at thisis.fryou@gmail.com",
+    ],
   },
-  bird:{title:"Bird",author:"Noy Holland", body:[['The average writer, even a great writer, tends to lose focus of other elements when they turn to the sentence. Plot can go right out the window. Character and feeling tend to take a back seat. And for those of us that love this sentence-forward work this is all the better, it allows the reader too to luxuriate in the sounds and unfolding without having to keep track of the broader picture.'],['Here, in "Bird", Noy Holland has not only excelled on the sentence level, trotting out every trick in the book and inventing countless along the way, dancing across the page effortlessly so that the sounds and rhythm nearly fade into the background while being utterly upfront and dazzling, but also contracts a gut-wrenching portrait of a relationship at the edge of being and the life that is instilled with it, and torn apart by it.'],['"Mickey toppled his boots for Bird to stand on, on the clothes heaped at their feet. Now he was in her, disappearing, shade to shade, his cock like a bull`s in the shadow they cast. Bird slickened with blood she was losing still; on her breasts, hieroglyphs of his hands. Mineral seep. Her feet were pewter; a beetle wandered in the swales of her tendons, daubing methodically at the spatter of her blood. A speckled wind, iridescent. Nothing more moved buck Mickey, Bird-a shadow fused, a Gorgon`s head."'],['Much of the books rides this ledge between waking and dreaming, between the mundane and the mystical, these hints of memory and nostalgia and imagination seeping in, but the hard world always there, the hard pain and ecstasy of Bird`s being wrought like cold wood on the page.'],['"Her jaw was swelling; it was yellowing and blue.'],
-['They made green together, yellow and blue. Blue and red made purple.'],
-['And what did yellow and Bird make?'],
-['And what did Mickey and Blue?'],
-['`And Mickey and Bird?`` Mickey asked.'],
-['And Bird said, `A bloody stew.``"'],["'Bird' has an unsentimental sentimentality, the love Bird feels for her son, for her baby, for Mickey so bone achingly deep, so strong it causes her to be drunk into sobriety, the clarity of the world turned up to the point of pain. Bird is neither victim nor perpetrator but raw vessel for the feeling of the world, a world so bright with pain and joy and love and loss that it warps the words within it, melts them together and causes them to kaleidoscope out, the only way it, and those within it, can be understood."]]},
+  directory: {
+    title: "Directory",
+    author: "Christopher Linforth",
+    purchase_link:
+      "https://www.spdbooks.org/Products/9789980243058/directory.aspx",
+    pub_info: "Pub Info",
+    body: [
+      [
+        "In its forty-one stories set out in proper, unadorned prose ‘Directory’ sets forward the thesis ‘What is it to be I versus we?’",
+      ],
+      [
+        "Each story in ‘Directory’ appears separate and self contained, most, koan-like, are a page or less in length. Each is a brick that builds a world that is similar to our own but darker, less forgiving and cloaked in an ominous light. There are through-lines in many of the stories, little details or personality traits that might link the unnamed characters, or which may be nothing more than coincidence.",
+      ],
+      [
+        "Long-stay motels, clapboard houses, rotting food, loose cigarettes and cramped rooms populate the world in which the stories of ‘Directory’ take place. The dark suburban glut. The boredom of childhood in a world of ease. The tension and frustration and bright joy of finding some way to feel some feeling.",
+      ],
+      [
+        "The characters, unnamed, often twins, ache through their symptoms and diseases, often of hereditary origin. Identity is fluid, the barriers between and within malleable or non-existent. Twins or siblings speak in the singular, some individuals, like the old caretaker in ‘Proposal’, speak in the plural which give the stories the gauzy, loaded feel of dreaming. They attend schools with circus clubs, at which they fail miserably. The frustration of their lives sublimates into self-destructive acts ‘…we flung darts at squirrels, jumped off one-story rooftops, made the crazed neighborhood pit bull chase us.’ They tell us, ‘We’re really here to talk about our virtues.’ but when their virtues are shown to be non-existent qualify: ‘So perhaps we don’t have virtues. But surely we have something.’ What is this something that they have? ‘Directory’ presents childhood in its true light, as something not bucolic or wondrous but rather filled with fear and the joy at spreading that fear to others.",
+      ],
+      [
+        "When they aren’t absent adults are often aggressors, antagonists and abusers. Perpetually clamping down on the destructive fun the children seek or seeking their own pleasure as they destroy the children themselves. Regardless, the blame is always placed on the children as the locus of control, the fount of evil.",
+      ],
+      [
+        "No one is free of guilt or sin. A neighbor who chastises two boys for setting off fireworks in himself found to be a murderer of dogs. In ‘Apocrypha’ a pair of twins are lured into their elderly neighbor’s house after the man claims that there is an intruder inside. After the twins enter a strange, bare room the neighbor leaves suddenly, announcing that the house isn't even his.",
+      ],
+      [
+        "Each story starts anew, with characters that exhibit a grinding familiarity, as if the same trauma is being relived again and again, the same escape attempted over and over yet never achieved. Absent mothers hold the elusive freedom that the main characters always long for, yet which is always out of reach. Abusive stepfathers, entranced with their own haphazard and self-serving religious systems occur again and again, always on the edge of the narrative, waiting for the reader to depart so he can again enact his torturous deeds.",
+      ],
+      [
+        "Even when escape appears to have been successful, as in ‘Tongues’, the abuser always lurks off in the shadows: ‘A final New Year, a millennium celebration, brought our stepfather back.’ Escape ultimately has its own cost, as in the case of ‘Tongues’ where the characters lose the things that had kept them whole. When revenge comes it is never sweet or cathartic, simply an act that must occur, as the sisters in ‘Tongues’ find, when, after cornering their abusive step father, and mutilating him they find only that ‘He spoke something, in the end, but we do not know what he said.’ Does he, when he finally faces the costs of his actions, apologize or simply continue to defend his actions?",
+      ],
+      [
+        "With all the darkness some humor does pop through, as in one instance where the mother is killed by a falling tree and, a neighbor '…sawed the trunk into neat circles. Wafer-thin slices he sold as flying discs, replacements for vinyl.'",
+      ],
+      [
+        "Religion is a common theme, always the feeble reflex of the dead and dying, or a dime store scam, like the preacher whose words or recorded by his followers and sold for ‘for $9.95 in Tompkins Square Park.’ When his church crumbles to the ground it reveals a shining monolith that causes the surrounding neighborhood to gentrify rapidly",
+      ],
+      [
+        "By the end of ‘Directory’ no easy answers to the ‘I/We’ conundrum have been set out. Rather, the reader comes away from the collection with a broader, more nuanced, even mystical view of identity, of humanity, of the world.",
+      ],
+    ],
+  },
+  bird: {
+    title: "Bird",
+    author: "Noy Holland",
+    body: [
+      [
+        "The average writer, even a great writer, tends to lose focus of other elements when they turn to the sentence. Plot can go right out the window. Character and feeling tend to take a back seat. And for those of us that love this sentence-forward work this is all the better, it allows the reader too to luxuriate in the sounds and unfolding without having to keep track of the broader picture.",
+      ],
+      [
+        'Here, in "Bird", Noy Holland has not only excelled on the sentence level, trotting out every trick in the book and inventing countless along the way, dancing across the page effortlessly so that the sounds and rhythm nearly fade into the background while being utterly upfront and dazzling, but also contracts a gut-wrenching portrait of a relationship at the edge of being and the life that is instilled with it, and torn apart by it.',
+      ],
+      [
+        '"Mickey toppled his boots for Bird to stand on, on the clothes heaped at their feet. Now he was in her, disappearing, shade to shade, his cock like a bull`s in the shadow they cast. Bird slickened with blood she was losing still; on her breasts, hieroglyphs of his hands. Mineral seep. Her feet were pewter; a beetle wandered in the swales of her tendons, daubing methodically at the spatter of her blood. A speckled wind, iridescent. Nothing more moved buck Mickey, Bird-a shadow fused, a Gorgon`s head."',
+      ],
+      [
+        "Much of the books rides this ledge between waking and dreaming, between the mundane and the mystical, these hints of memory and nostalgia and imagination seeping in, but the hard world always there, the hard pain and ecstasy of Bird`s being wrought like cold wood on the page.",
+      ],
+      ['"Her jaw was swelling; it was yellowing and blue.'],
+      ["They made green together, yellow and blue. Blue and red made purple."],
+      ["And what did yellow and Bird make?"],
+      ["And what did Mickey and Blue?"],
+      ["`And Mickey and Bird?`` Mickey asked."],
+      ['And Bird said, `A bloody stew.``"'],
+      [
+        "'Bird' has an unsentimental sentimentality, the love Bird feels for her son, for her baby, for Mickey so bone achingly deep, so strong it causes her to be drunk into sobriety, the clarity of the world turned up to the point of pain. Bird is neither victim nor perpetrator but raw vessel for the feeling of the world, a world so bright with pain and joy and love and loss that it warps the words within it, melts them together and causes them to kaleidoscope out, the only way it, and those within it, can be understood.",
+      ],
+    ],
+  },
   the_goners: {
     title: "The Goners",
     author: "Mark Gluth",
-    body:
-      [["Utterly beautiful. Utterly brilliant. Utterly wracked with pain. First, the sentences. These are all turned inside out, as if the world they represent is itself turned from within. They slow your reading, cause you to lurch and jitter in your reading like the ones they represent bolt and halt through their own lives. This is not some hack or trick but a pure representation, the psychopomp's bare narration. Then the characters themselves: burnt into their pain, placed into a world turned utterly against them at every turn. We are no different but we can only appreciate their starkness."], ["Not only this, they are forced to live their pain through repeated emanations, cut into their various avatars by some cruel and distant god. Even through death their is no solution, even in mourning there is no relief. Identity, joy, pain, death are held up like lightless crystals, their facets shining points of blinding illumination on some awful chambers of the heart. These stories are not linked, they are interwoven, some ingrown labyrinth, some nested prison. These stories grow slowly, imperceptibly, one within another ultimately into something so much grater than their parts. Stunning contemporary fiction. Challenging, beautiful and terribly singular."]],
+    body: [
+      [
+        "Utterly beautiful. Utterly brilliant. Utterly wracked with pain. First, the sentences. These are all turned inside out, as if the world they represent is itself turned from within. They slow your reading, cause you to lurch and jitter in your reading like the ones they represent bolt and halt through their own lives. This is not some hack or trick but a pure representation, the psychopomp's bare narration. Then the characters themselves: burnt into their pain, placed into a world turned utterly against them at every turn. We are no different but we can only appreciate their starkness.",
+      ],
+      [
+        "Not only this, they are forced to live their pain through repeated emanations, cut into their various avatars by some cruel and distant god. Even through death their is no solution, even in mourning there is no relief. Identity, joy, pain, death are held up like lightless crystals, their facets shining points of blinding illumination on some awful chambers of the heart. These stories are not linked, they are interwoven, some ingrown labyrinth, some nested prison. These stories grow slowly, imperceptibly, one within another ultimately into something so much grater than their parts. Stunning contemporary fiction. Challenging, beautiful and terribly singular.",
+      ],
+    ],
   },
   homesick_for_another_world: {
     title: "Homesick For Another World",
     author: "Otessa Moshfegh",
-    body:
-      ["Fantastic. Why are Moshfegh's short stories so incredible? Because she comes to points where other writers would languish or spin out or recoil in fear, where the story would get bogged down in over plotting or exposition and she (somehow) pushes through in majestic and unforseeable ways. Watching these moments is like seeing a rock climber come to an impassable flat spot on the rock face,and instead of shimmying aside or retracing their steps simply leaping over the spot. It's like seeing someone break the sound barrier for the first time."],
+    body: [
+      "Fantastic. Why are Moshfegh's short stories so incredible? Because she comes to points where other writers would languish or spin out or recoil in fear, where the story would get bogged down in over plotting or exposition and she (somehow) pushes through in majestic and unforseeable ways. Watching these moments is like seeing a rock climber come to an impassable flat spot on the rock face,and instead of shimmying aside or retracing their steps simply leaping over the spot. It's like seeing someone break the sound barrier for the first time.",
+    ],
   },
   the_dolls_alphabet: {
     title: "The Dolls Alphabet",
     author: "Camilla Grudova",
-    body:
-      ["Fantastic stuff. A mirror. Imagine that many things we take for granted in our world would horrify the inhabitants of Grudova's world(s) just as the things that they take for granted horrify us."],
+    body: [
+      "Fantastic stuff. A mirror. Imagine that many things we take for granted in our world would horrify the inhabitants of Grudova's world(s) just as the things that they take for granted horrify us.",
+    ],
   },
 };
 
