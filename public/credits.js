2033f2d572c41a209b1bc9b961831f62b232b976
Samuel Moss <samuel.moss@lab651.com>
2022-06-09 08:45:46 -0700

    adds minor literatures review to credits

--- public/credits.js
+++ public/credits.js
@@ -3,22 +3,25 @@
 //   quote: 'this is the quote'
 // }
 
-export const credits = [
-  { year: "2022" },
-  {
-    title: [
-      "[Forthcoming]",
-      <i> The Starsick </i>,
-      "produced as a podcast on The Wicked Library in early 2022",
-    ],
-    quote:
-      "“I still didn`t understand. I asked my grandmother what sort of star it was, a constellation or a shooting star or something else. “No. No, shooting star,” she said, thinking for a minute. “Like a star, but different. Like a black star. And they watch it for a long time. Only in certain places: it happens. It’s a bad thing. These children—” she shook her head.”",
-  },
-  {
-    title: [
-      <i>The Veldt Light</i>,
-      " in ",
-      <a href="https://www.newnewsinews.com/issue5/moss"> New Sinews #5</a>,
+export const credits = [{
+      year: "2022"
+    },
+    {
+      title: [
+        "[Forthcoming]", <
+        i > The Starsick < /i>,
+        "produced as a podcast on The Wicked Library in early 2022",
+      ],
+      quote: "“I still didn`t understand. I asked my grandmother what sort of star it was, a constellation or a shooting star or something else. “No. No, shooting star,” she said, thinking for a minute. “Like a star, but different. Like a black star. And they watch it for a long time. Only in certain places: it happens. It’s a bad thing. These children—” she shook her head.”",
+    },
+    {
+      title: [ < i > Indeterminacy is what keeps us alive and makes us real< /i>,", June 2022",<br / > , "An Interview with David Leo Rice", " in ", < a href = "https://minorliteratures.com/2022/06/07/indeterminacy-is-what-keeps-us-alive-and-makes-us-real-an-interview-with-david-leo-rice-samuel-m-moss/" > minor literatures[s] < /a>],
+        quote: "I interviewed David Leo Rice about his book 'The New House'"
+      },
+      {
+        title: [ < i > The Veldt Light < /i>,
+          " in ", <
+          a href = "https://www.newnewsinews.com/issue5/moss" > New Sinews #5</a>,
       ", March 2022",
       <br />,
       "An excerpt from 'The Veldt Institute' a novella in progress",
@@ -31,65 +34,64 @@ export const credits = [
     title: [
       <i> Lemma </i>,
       " in ",
-      <a href="http://www.wordforword.info/vol37/Moss.html">Word For/ Word</a>,
-      ,
-      ", July 2021",
-    ],
-    quote: "One poem in 'Word For/ Word'",
-  },
-  {
-    title: [
-      <i>Coming, Going</i>,
-      " in ",
-      <a href="https://newworldwriting.net/samuel-m-moss-coming-going/">
-        New World Writing
-      </a>,
-      ", June 2021",
-    ],
-    quote: `"Were he to have the heart surgery nothing would change. The mornings could come and the evenings would come. The children would cry, still."`,
-  },
-  {
-    title: [
-      <i> Morello, Van, Marasca in </i>,
-      <a href="https://www.3ammagazine.com/3am/morello-van-marasca/">
-        3:AM Magazine
-      </a>,
-      ", June 2021",
-    ],
-    quote:
-      "“One thing I’ve learned,” she’d said one morning, unbidden, “Is that if you talk enough about something you don’t have, it will eventually find you.” I had to wonder if that was why she never talked of stuff.",
-  },
+      <a href= "http://www.wordforword.info/vol37/Moss.html" > Word For / Word < /a>,,
+          ", July 2021",
+        ],
+        quote: "One poem in 'Word For/ Word'",
+      },
+      {
+        title: [ <
+          i > Coming, Going < /i>,
+          " in ", <
+          a href = "https://newworldwriting.net/samuel-m-moss-coming-going/" >
+          New World Writing <
+          /a>,
+          ", June 2021",
+        ],
+        quote: `"Were he to have the heart surgery nothing would change. The mornings could come and the evenings would come. The children would cry, still."`,
+      },
+      {
+        title: [ <
+          i > Morello, Van, Marasca in < /i>, <
+          a href = "https://www.3ammagazine.com/3am/morello-van-marasca/" >
+          3: AM Magazine <
+          /a>,
+          ", June 2021",
+        ],
+        quote: "“One thing I’ve learned,” she’d said one morning, unbidden, “Is that if you talk enough about something you don’t have, it will eventually find you.” I had to wonder if that was why she never talked of stuff.",
+      },
 
-  {
-    title: [
-      <i> The Sculptor </i>,
-      "in ",
-      <a href="https://grimscribepress.com/issues/">
-        Vastarien: A Literary Journal
-      </a>,
-      " vol.4 issue 1, Spring 2021",
-    ],
-    quote:
-      "A Journalist gets an invitation to interview a reclusive sculptor and learns firsthand the meaning of the artist`s perplexing work.",
-  },
-  {
-    title: [
-      <i>The Catch </i>,
-      "in",
-      <a href="https://www.youngmag.io/article/the-catch"> Young Mag </a>,
-      ", January 2021",
-    ],
-    quote:
-      '"There’s a place nearby that sells these little bottles. Script unintelligible. It’s all caffeine with a dash of cholesterol and casein. You drink it down in a gulp and the whole world gets wan and jiggly. Things seem slick, but manageable. Whole barriers of the mind are broken down and you feel like you can talk to anyone, anywhere, anytime. I wash three of these a day down my gullet, civet scented, I think."',
-  },
-  { year: "2020" },
-  {
-    title: [
-      <i>Gallaher Calls </i>,
-      "in",
-      <a href="https://dimshores.bigcartel.com/product/dim-shores-presents-volume-1-anthology-2nd-edition">
-        {" "}
-        Dim Shores Presents #1'{" "}
+      {
+        title: [ <
+          i > The Sculptor < /i>,
+          "in ", <
+          a href = "https://grimscribepress.com/issues/" >
+          Vastarien: A Literary Journal <
+          /a>,
+          " vol.4 issue 1, Spring 2021",
+        ],
+        quote: "A Journalist gets an invitation to interview a reclusive sculptor and learns firsthand the meaning of the artist`s perplexing work.",
+      },
+      {
+        title: [ <
+          i > The Catch < /i>,
+          "in", <
+          a href = "https://www.youngmag.io/article/the-catch" > Young Mag < /a>,
+          ", January 2021",
+        ],
+        quote: '"There’s a place nearby that sells these little bottles. Script unintelligible. It’s all caffeine with a dash of cholesterol and casein. You drink it down in a gulp and the whole world gets wan and jiggly. Things seem slick, but manageable. Whole barriers of the mind are broken down and you feel like you can talk to anyone, anywhere, anytime. I wash three of these a day down my gullet, civet scented, I think."',
+      },
+      {
+        year: "2020"
+      },
+      {
+        title: [ <
+          i > Gallaher Calls < /i>,
+          "in", <
+          a href = "https://dimshores.bigcartel.com/product/dim-shores-presents-volume-1-anthology-2nd-edition" > {
+            " "
+          }
+          Dim Shores Presents #1'{" "}
       </a>,
       ", June 2020",
     ],
@@ -103,36 +105,38 @@ export const credits = [
         Steven Purtill{" "}
       </i>,
       "on",
-      <a href="https://denniscooperblog.com/please-welcome-to-the-world-mark-gluth-come-down-to-us-mark-gluth-and-steven-purtill-crippled-symmetry-kiddiepunk-press/">
-        {" "}
-        Dennis Cooper's Blog{" "}
-      </a>,
-    ],
-    quote:
-      "An interview with Author Mark Gluth about his novel `Come Down to Us` as well as an interview with artist Steven Purtill who collaborated with Mark on projects related to the novel.",
-  },
-  { year: "2019" },
-  {
-    title: [
-      <i>Drug Lung </i>,
-      "and",
-      <i> Some Eastern Demon </i>,
-      "in",
-      <a href="http://www.decompmagazine.com/mossflashes.htm"> decomP </a>,
-      ", Sep 2019",
-    ],
-    quote:
-      "I saw some eastern demon crawling. Called out, Catch as catch can! and started to run. He was lumpen, tired, smoking a cigarette. Sweatpants and derisive comments. I made it thirty feet before turning. These things don’t pay as well as they used to, he said.",
-  },
-  { year: "2014" },
-  {
-    title: [
-      <i>Lying Down Machines </i>,
-      "in",
-      <a href="http://journal.gonelawn.net/issue16/Moss.php"> Gone Lawn 16 </a>,
-      ", Fall 2014",
-    ],
-    quote:
-      "The father and the son built the machines from boards they gathered on the Taiga. It took two months to build the first Lying Down Machine. They made black pine pitch in a fire hut and gathered Widow`s Teeth from within the granite crevices.",
-  },
-];
+      <a href= "https://denniscooperblog.com/please-welcome-to-the-world-mark-gluth-come-down-to-us-mark-gluth-and-steven-purtill-crippled-symmetry-kiddiepunk-press/" > {
+            " "
+          }
+          Dennis Cooper 's Blog{" "} <
+          /a>,
+        ],
+        quote: "An interview with Author Mark Gluth about his novel `Come Down to Us` as well as an interview with artist Steven Purtill who collaborated with Mark on projects related to the novel.",
+      },
+      {
+        year: "2019"
+      },
+      {
+        title: [ <
+          i > Drug Lung < /i>,
+          "and", <
+          i > Some Eastern Demon < /i>,
+          "in", <
+          a href = "http://www.decompmagazine.com/mossflashes.htm" > decomP < /a>,
+          ", Sep 2019",
+        ],
+        quote: "I saw some eastern demon crawling. Called out, Catch as catch can! and started to run. He was lumpen, tired, smoking a cigarette. Sweatpants and derisive comments. I made it thirty feet before turning. These things don’t pay as well as they used to, he said.",
+      },
+      {
+        year: "2014"
+      },
+      {
+        title: [ <
+          i > Lying Down Machines < /i>,
+          "in", <
+          a href = "http://journal.gonelawn.net/issue16/Moss.php" > Gone Lawn 16 < /a>,
+          ", Fall 2014",
+        ],
+        quote: "The father and the son built the machines from boards they gathered on the Taiga. It took two months to build the first Lying Down Machine. They made black pine pitch in a fire hut and gathered Widow`s Teeth from within the granite crevices.",
+      },
+    ];
