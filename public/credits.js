dc7989a5bb578bcccf406ac9d676045d61e53a9c
Samuel Moss <samuel.moss@lab651.com>
2024-08-30 07:23:26 -0700

    adds minor li[s]t, Chthonic Matter to credits

--- public/credits.js
+++ public/credits.js
@@ -3,134 +3,213 @@
 //   quote: 'this is the quote'
 // }
 
-export const credits = [{year: "2024"},{title: [<i>Dead Men's Fingernails</i>," in Fabulist Flash (Forthcoming)"]},{
-  year: "2023"
-},{
-  title: ["Two Stories: ", <i>Near Where the Rector Had Stood</i>, " and ", <i>My Program</i>,
-    " on ", <a href="https://minorliteratures.com/2023/03/30/two-stories-samuel-m-moss/">minor literature[s]</a>, ", March 2023"
-  ],
-  quote: "I had to get away from my program, which had turned stifling, so I went to the famous church on the edge of campus. The church was known for being very old, and the group with which it was associated known for being very pious.",
-},{
-  year: "2022"
-},
-{
-  title: [<i> The Starsick </i>,
-    "produced as a podcast on ", <a href="https://www.spreaker.com/user/8992719/ew-2022-2-128">The Wicked Library</a>,", October 2022",
-  ],
-  quote: "“I still didn`t understand. I asked my grandmother what sort of star it was, a constellation or a shooting star or something else?” “No. No, shooting star,” she said, thinking for a minute. “Like a star, but different. Like a black star. And they watch it for a long time. Only in certain places: it happens. It’s a bad thing. These children—” she shook her head.”",
-},
-{
-  title: [< i > Indeterminacy is what keeps us alive and makes us real< /i>,", June 2022",<br /> , "An Interview with David Leo Rice", " in ", < a href="https://minorliteratures.com/2022/06/07/indeterminacy-is-what-keeps-us-alive-and-makes-us-real-an-interview-with-david-leo-rice-samuel-m-moss/" > minor literatures[s] < /a>],
-    quote: "I interviewed David Leo Rice about his book 'The New House'"
+export const credits = [
+  { year: "2025" },
+  { title: [<i>A Paper Game Teacher</i>, " in Chthonic Matter (Forthcoming)"] },
+  {
+    title: [<i>Dead Men's Fingernails</i>, " in Fabulist Flash (Forthcoming)"],
   },
-      {
-      title: [ < i > The Veldt Light < /i>,
-          " in ", <
-        a href="https://www.newnewsinews.com/issue5/moss" > New Sinews #5</a>,
-  ", March 2022",
+  { year: "2024" },
+  {
+    title: [<i>Debut Full-length</i>, " (Forthcoming)"],
+  },
+  {
+    title: [
+      <i>Entry on J.M. Coetzee's 'Life of Jesus' Trilogy</i>,
+      " in minor li[s]t (Forthcoming)",
+    ],
+  },
+  {
+    year: "2023",
+  },
+  {
+    title: [
+      "Two Stories: ",
+      <i>Near Where the Rector Had Stood</i>,
+      " and ",
+      <i>My Program</i>,
+      " on ",
+      <a href="https://minorliteratures.com/2023/03/30/two-stories-samuel-m-moss/">
+        minor literature[s]
+      </a>,
+      ", March 2023",
+    ],
+    quote:
+      "I had to get away from my program, which had turned stifling, so I went to the famous church on the edge of campus. The church was known for being very old, and the group with which it was associated known for being very pious.",
+  },
+  {
+    year: "2022",
+  },
+  {
+    title: [
+      <i> The Starsick </i>,
+      "produced as a podcast on ",
+      <a href="https://www.spreaker.com/user/8992719/ew-2022-2-128">
+        The Wicked Library
+      </a>,
+      ", October 2022",
+    ],
+    quote:
+      "“I still didn`t understand. I asked my grandmother what sort of star it was, a constellation or a shooting star or something else?” “No. No, shooting star,” she said, thinking for a minute. “Like a star, but different. Like a black star. And they watch it for a long time. Only in certain places: it happens. It’s a bad thing. These children—” she shook her head.”",
+  },
+  {
+    title: [
+      <i> Indeterminacy is what keeps us alive and makes us real</i>,
+      ", June 2022",
+      <br />,
+      "An Interview with David Leo Rice",
+      " in ",
+      <a href="https://minorliteratures.com/2022/06/07/indeterminacy-is-what-keeps-us-alive-and-makes-us-real-an-interview-with-david-leo-rice-samuel-m-moss/">
+        {" "}
+        minor literatures[s]{" "}
+      </a>,
+    ],
+    quote: "I interviewed David Leo Rice about his book 'The New House'",
+  },
+  {
+    title: [
+      <i> The Veldt Light </i>,
+      " in ",
+      <a href="https://www.newnewsinews.com/issue5/moss"> New Sinews #5</a>,
+      ", March 2022",
       <br />,
       "An excerpt from 'The Veldt Institute' a novella in progress",
     ],
     quote:
       "The light is itself a healer, or perhaps better said, a teacher. The sun rises every morning in the east and casts its rays and long shadows toward the west for an hour or so. Rapidly the sun rises in the sky until it seems to hang directly above the Institute—that is to say, in the very center of the veldt itself—at which point all shadows diminish to nothing.",
   },
-  {year: "2021" },
+  { year: "2021" },
   {
-        title: [
+    title: [
       <i> Lemma </i>,
       " in ",
-      <a href="http://www.wordforword.info/vol37/Moss.html" > Word For / Word < /a>,,
-          ", July 2021",
-        ],
-        quote: "One poem in 'Word For/ Word'",
-      },
-      {
-          title: [ <
-          i > Coming, Going < /i>,
-          " in ", <
-          a href="https://newworldwriting.net/samuel-m-moss-coming-going/" >
-            New World Writing <
-          /a>,
-            ", June 2021",
-          ],
-          quote: `"Were he to have the heart surgery nothing would change. The mornings could come and the evenings would come. The children would cry, still."`,
-        },
-      {
-              title: [ <
-          i > Morello, Van, Marasca in < /i>, <
-          a href="https://www.3ammagazine.com/3am/morello-van-marasca/" >
-                3: AM Magazine <
-          /a>,
-                ", June 2021",
-              ],
-              quote: "“One thing I’ve learned,” she’d said one morning, unbidden, “Is that if you talk enough about something you don’t have, it will eventually find you.” I had to wonder if that was why she never talked of stuff.",
-            },
-
-      {
-                  title: [ <
-          i > The Sculptor < /i>,
-          "in ", <a href="https://grimscribepress.com/issues/" >Vastarien: A Literary Journal </a>,
-                    " vol.4 issue 1, Spring 2021",
-                    <a href="https://ellendatlow.com/2022/11/28/recommendations-for-2014-long-list-2/" > (BHOtY #14 longlisted)</a>,
-                  ],
-                  quote: "A Journalist gets an invitation to interview a reclusive sculptor and learns firsthand the meaning of the artist`s perplexing work.",
-                },
-      {
-                      title: [ <
-          i > The Catch < /i>,
-          "in", <
-          a href="https://www.youngmag.io/article/the-catch" > Young Mag, < /a>,
-                        " January 2021",
-                      ],
-                      quote: '"There’s a place nearby that sells these little bottles. Script unintelligible. It’s all caffeine with a dash of cholesterol and casein. You drink it down in a gulp and the whole world gets wan and jiggly. Things seem slick, but manageable. Whole barriers of the mind are broken down and you feel like you can talk to anyone, anywhere, anytime. I wash three of these a day down my gullet, civet scented, I think."',
-                    },
-      {
-                          year: "2020"
-                      },
-      {
-                          title: [ <
-          i > Gallaher Calls < /i>,
-          "in", <
-                            a href="https://dimshores.bigcartel.com/product/dim-shores-presents-volume-1-anthology-2nd-edition" > {
-                              " "
-                            }
-                            Dim Shores Presents #1'{" "}
-                          </a>,
-                          ", June 2020",
-                        ],
-                        quote:
-                          "Two reclusive siblings live in the shadow of the shadow of their family`s wealth, creating art that no one sees. When the family lawyer Gallaher calls to deliver some sorry news the lavish the hermetic and aesthetic environment they have carefully curated goes awry. What is real, what is imagined; what is dreamt and what is created all fall together.",
-                      },
+      <a href="http://www.wordforword.info/vol37/Moss.html">
+        {" "}
+        Word For / Word{" "}
+      </a>,
+      ,
+      ", July 2021",
+    ],
+    quote: "One poem in 'Word For/ Word'",
+  },
   {
-                            title: [
+    title: [
+      <i> Coming, Going </i>,
+      " in ",
+      <a href="https://newworldwriting.net/samuel-m-moss-coming-going/">
+        New World Writing{" "}
+      </a>,
+      ", June 2021",
+    ],
+    quote: `"Were he to have the heart surgery nothing would change. The mornings could come and the evenings would come. The children would cry, still."`,
+  },
+  {
+    title: [
+      <i> Morello, Van, Marasca in </i>,
+      <a href="https://www.3ammagazine.com/3am/morello-van-marasca/">
+        3: AM Magazine{" "}
+      </a>,
+      ", June 2021",
+    ],
+    quote:
+      "“One thing I’ve learned,” she’d said one morning, unbidden, “Is that if you talk enough about something you don’t have, it will eventually find you.” I had to wonder if that was why she never talked of stuff.",
+  },
+
+  {
+    title: [
+      <i> The Sculptor </i>,
+      "in ",
+      <a href="https://grimscribepress.com/issues/">
+        Vastarien: A Literary Journal{" "}
+      </a>,
+      " vol.4 issue 1, Spring 2021",
+      <a href="https://ellendatlow.com/2022/11/28/recommendations-for-2014-long-list-2/">
+        {" "}
+        (BHOtY #14 longlisted)
+      </a>,
+    ],
+    quote:
+      "A Journalist gets an invitation to interview a reclusive sculptor and learns firsthand the meaning of the artist`s perplexing work.",
+  },
+  {
+    title: [
+      <i> The Catch </i>,
+      "in",
+      <a href="https://www.youngmag.io/article/the-catch"> Young Mag, </a>,
+      " January 2021",
+    ],
+    quote:
+      '"There’s a place nearby that sells these little bottles. Script unintelligible. It’s all caffeine with a dash of cholesterol and casein. You drink it down in a gulp and the whole world gets wan and jiggly. Things seem slick, but manageable. Whole barriers of the mind are broken down and you feel like you can talk to anyone, anywhere, anytime. I wash three of these a day down my gullet, civet scented, I think."',
+  },
+  {
+    year: "2020",
+  },
+  {
+    title: [
+      <i> Gallaher Calls </i>,
+      "in",
+      <a href="https://dimshores.bigcartel.com/product/dim-shores-presents-volume-1-anthology-2nd-edition">
+        {" "}
+        Dim Shores Presents #1'{" "}
+      </a>,
+      ", June 2020",
+    ],
+    quote:
+      "Two reclusive siblings live in the shadow of the shadow of their family`s wealth, creating art that no one sees. When the family lawyer Gallaher calls to deliver some sorry news the lavish the hermetic and aesthetic environment they have carefully curated goes awry. What is real, what is imagined; what is dreamt and what is created all fall together.",
+  },
+  {
+    title: [
       <i>
-                            Some Other and Separate Iteration: An Interview with Mark Gluth and
+        Some Other and Separate Iteration: An Interview with Mark Gluth and
         Steven Purtill{" "}
-                          </i>,
-                          "on",
-      <a href="https://denniscooperblog.com/please-welcome-to-the-world-mark-gluth-come-down-to-us-mark-gluth-and-steven-purtill-crippled-symmetry-kiddiepunk-press/" > {
-                            " "
-                          }
-                            Dennis Cooper 's Blog{" "} <
-          /a>,
-                          ],
-                          quote: "An interview with Author Mark Gluth about his novel `Come Down to Us` as well as an interview with artist Steven Purtill who collaborated with Mark on projects related to the novel.",
-                        },
-      {year: "2019"},
-      {
-        title: [ <i > Drug Lung < /i>,"and", <i > Some Eastern Demon < /i>,"in", <a href="http://www.decompmagazine.com/mossflashes.htm" > decomP< /a>,", Sep 2019",],
-        quote: "I saw some eastern demon crawling. Called out, Catch as catch can! and started to run. He was lumpen, tired, smoking a cigarette. Sweatpants and derisive comments. I made it thirty feet before turning. These things don’t pay as well as they used to, he said.",},
-      {
-        title: [ <i> The Plague Victim </i>,"in", <a href="https://chthonicmatter.wordpress.com/nightscript/" > Nightscript #5< /a>,", Oct 2019",],
-        quote: "In the general chaos following the fleeing of the livestock the man sank into a violent silence.",},
-      {year: "2014"},
-      {
-                                    title: [ <
-          i > Lying Down Machines < /i>,
-          "in", <
-          a href="https://gonelawn.net/journal/issue16/Moss.php" > Gone Lawn 16 < /a>,
-                                      ", Fall 2014",
-                                    ],
-                                    quote: "The father and the son built the machines from boards they gathered on the Taiga. It took two months to build the first Lying Down Machine. They made black pine pitch in a fire hut and gathered Widow`s Teeth from within the granite crevices.",
-                                  },
-                                ];
+      </i>,
+      "on",
+      <a href="https://denniscooperblog.com/please-welcome-to-the-world-mark-gluth-come-down-to-us-mark-gluth-and-steven-purtill-crippled-symmetry-kiddiepunk-press/">
+        {" "}
+        Dennis Cooper 's Blog{" "}
+      </a>,
+    ],
+    quote:
+      "An interview with Author Mark Gluth about his novel `Come Down to Us` as well as an interview with artist Steven Purtill who collaborated with Mark on projects related to the novel.",
+  },
+  { year: "2019" },
+  {
+    title: [
+      <i> Drug Lung </i>,
+      "and",
+      <i> Some Eastern Demon </i>,
+      "in",
+      <a href="http://www.decompmagazine.com/mossflashes.htm"> decomP</a>,
+      ", Sep 2019",
+    ],
+    quote:
+      "I saw some eastern demon crawling. Called out, Catch as catch can! and started to run. He was lumpen, tired, smoking a cigarette. Sweatpants and derisive comments. I made it thirty feet before turning. These things don’t pay as well as they used to, he said.",
+  },
+  {
+    title: [
+      <i> The Plague Victim </i>,
+      "in",
+      <a href="https://chthonicmatter.wordpress.com/nightscript/">
+        {" "}
+        Nightscript #5
+      </a>,
+      ", Oct 2019",
+    ],
+    quote:
+      "In the general chaos following the fleeing of the livestock the man sank into a violent silence.",
+  },
+  { year: "2014" },
+  {
+    title: [
+      <i> Lying Down Machines </i>,
+      "in",
+      <a href="https://gonelawn.net/journal/issue16/Moss.php">
+        {" "}
+        Gone Lawn 16{" "}
+      </a>,
+      ", Fall 2014",
+    ],
+    quote:
+      "The father and the son built the machines from boards they gathered on the Taiga. It took two months to build the first Lying Down Machine. They made black pine pitch in a fire hut and gathered Widow`s Teeth from within the granite crevices.",
+  },
+];
