5a82078ad1f467386bfc777130f56bfe76d40e89
Samuel Moss <samuel.moss@lab651.com>
2025-02-14 08:05:00 -0700

    adds new author photo

--- pages/bio.js
+++ pages/bio.js
@@ -17,17 +17,22 @@ export default function Bio() {
         <Header />
         <div className={styles.bio}>
           <div className={styles.bioImage}>
-            <Image src="/bio_img.jpg" width="300" height="300" />
+            <Image src="/images/2025_photo.jpg" width="240" height="300" />
+            <div class="authorPhotoCredit">
+              Author photo by{" "}
+              <Link href="https://www.instagram.com/doomstache">
+                Jon R. Moore
+              </Link>
+            </div>
           </div>
           <div className={styles.bioBody}>
-            Samuel M. Mossis a writer and editor who lives in rural Cascadia.
+            Samuel M. Moss is a writer and editor who lives in rural Cascadia.
             His fiction and poetry have been published in <i>3:AM Magazine</i>,{" "}
             <i>minor literature[s]</i>, <i>New World Writing</i>,{" "}
             <i>Word For/ Word</i>, <i>Dim Shores Presents</i>, and{" "}
             <i>Vastarien</i> <Link href="credits">among other venues</Link>. He
             runs <a href="https://www.ergot.press/">ergot.</a>, a site for
-            innovative and experimental horror and is an associate editor and
-            web lead at <a href="https://1111press.com/">11:11 Press</a>.
+            innovative and experimental horror.
           </div>
         </div>
       </div>
--- pages/index.js
+++ pages/index.js
@@ -3,26 +3,36 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import Header from "../components/Header.js";
+import { Helmet } from "react-helmet";
 
 export default function Home() {
   return (
     <>
+      <Helmet>
+        <script type="application/ld+json">
+          {`{"@context":"https://schema.org","@type":"Person","name":"Samuel M. Moss","maintainer":"ergot. press"}`}
+        </script>
+      </Helmet>
       <div className={styles.content}>
         <Header />
         <div className={styles.bio}>
-          <Image
-            className={styles.bioImage}
-            src="/bio_img.jpg"
-            width="300"
-            height="300"
-          />
+          <div className={styles.bioImage}>
+            <Image src="/images/2025_photo.jpg" width="240" height="300" />
+            <div class="authorPhotoCredit">
+              Author photo by{" "}
+              <Link href="https://www.instagram.com/doomstache">
+                Jon R. Moore
+              </Link>
+            </div>
+          </div>
           <div className={styles.bioBody}>
-            Samuel M. Moss is from Cascadia. He has work published, or
-            forthcoming, in <i>3:AM Magazine</i>, <i>Young Mag</i>,{" "}
-            <i>Word For/ Word</i> and <i>Vastarien</i>{" "}
-            <Link href="credits">among other venues</Link>. He is an associate
-            editor and web lead at{" "}
-            <a href="https://1111press.com/">11:11 Press</a>.
+            Samuel M. Moss is a writer and editor who lives in rural Cascadia.
+            His fiction and poetry have been published in <i>3:AM Magazine</i>,{" "}
+            <i>minor literature[s]</i>, <i>New World Writing</i>,{" "}
+            <i>Word For/ Word</i>, <i>Dim Shores Presents</i>, and{" "}
+            <i>Vastarien</i> <Link href="credits">among other venues</Link>. He
+            runs <a href="https://www.ergot.press/">ergot.</a>, a site for
+            innovative and experimental horror.
           </div>
         </div>
       </div>
